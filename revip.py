bcd8d98e1213f30052a4b8ea1de021d25eb3f498
Bibib <nalek288@gmail.com>
2021-02-27 01:01:17 +0800

    Update

--- revip.py
+++ revip.py
@@ -1,47 +1,58 @@
 #!/usr/bin/env python
 
 import argparse
-import os
 import requests
+import os
 
 parser = argparse.ArgumentParser(description='Reverse IP Lookup')
 parser.add_argument('-l', '--list', metavar='', type=str, required=True, help='file path of the lists of IP address')
 parser.add_argument('-s', '--save', metavar='', type=str, required=True, help='file path to save the results')
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-v', '--verbose', action='store_true', help='print the verbose')
+parser.add_argument('-v', '--verbose', action='store_true', help='print the verbose')
 args = parser.parse_args()
 
 counter = []
+req = requests.Session()
+
+def clean_ip(list):
+	clean_list = []
+	with open(list, 'r') as f:
+		ips = f.readlines()
+		for ip in ips:
+			if ip.strip() not in clean_list:
+				clean_list.append(ip.strip())
+		
+	return clean_list
 
 def revip(list, save, verbose=False):
-	try:
-		with open(list, 'r') as f:
-			ips = f.readlines()
-			for ip in ips:
-				if verbose:
-					print('\nTrying IP', ip.strip())
-					print('Connecting to API...')
-				url = 'https://sonar.omnisint.io/reverse/' + ip.strip()
-				s = requests.get(url)
-				if s.text.strip() != 'null':
-					resp = s.json()
-					count_per_ip = len(resp)
-					counter.append(count_per_ip)
-					for i in resp:
-						with open(save, 'a+') as res:
-							res.write(i + '\n')
-					if verbose:
-						print('Get', count_per_ip, 'reversed from IP', ip.strip())
-				else:
-					if verbose:
-						print(ip.strip(), 'Null results')
-			print('\nReverse completed!')
+	ips = clean_ip(list)
+	for ip in ips:
+		if verbose:
+			print('\nReversing IP', ip)
+			print('Connecting to API...')
+		url = 'https://sonar.omnisint.io/reverse/' + ip
+		s = req.get(url)
+		if s.text.strip() != 'null':
+			resp = s.json()
+			count_per_ip = len(resp)
+			counter.append(count_per_ip)
+			for i in resp:
+				with open(save, 'a+') as res:
+					res.write(i + '\n')
 			if verbose:
-				print('Success get', sum(counter), 'from', len(ips), 'lists')
-			print('Results save as', save)
-	except FileNotFoundError:
-		print(f'\nFile {list} notfound.')
-		os.sys.exit()
-
+				print('Get', count_per_ip, 'reversed from IP', ip)
+		else:
+			if verbose:
+				print('Get null reversed from IP', ip)
+	print('\nReverse completed!')
+	if verbose:
+		print('Success get', sum(counter), 'from', len(ips), 'lists')
+	print('Results save as', save)
+		
 if __name__ == '__main__':
-	revip(args.list, args.save, args.verbose)
+	print('Trying open', args.list)
+	try:
+		open(args.list, 'r')
+		revip(args.list, args.save, args.verbose)
+	except FileNotFoundError:
+		print(f'\nFile {args.list} notfound.')
+		os.sys.exit()
